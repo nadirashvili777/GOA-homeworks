c6288cead20f0495a8a4c22546556773246d9965
nadirashvili777 <nikoloznadirashvili08@gmail.com>
2025-06-19 19:18:51 +0400

    commit

---
+++ day 06/classwork/classwork.py
@@ -0,0 +1,4 @@
+name=input("please input your name: ")
+surname=input("please input your surname: ")
+age=input("please input your age: ")
+print("მე ვარ " + name + " " + surname + " " + age + " წლის ")
